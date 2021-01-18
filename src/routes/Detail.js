21e4d0b831bbdd04289768acf1e8dcbfeaab0617
dal <res1206@naver.com>
2021-01-18 19:51:49 +0900

    최종

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
     componentDidMount() {
@@ -10,7 +11,19 @@ class Detail extends React.Component {
     render() {
         const { location } = this.props;
         if (location.state) {
-            return <span>{location.state.title}</span>;
+            return (
+                <div className="detail">
+                    <img src={location.state.poster}/>
+                    <h1>{location.state.title}</h1>
+                    <h3>{location.state.year}</h3>
+                    <ul>
+                        {location.state.genres.map((genre, index) => (
+                            <li key={index}>{genre}</li>
+                        ))}
+                    </ul>
+                    <h4>{location.state.summary}</h4>
+                </div>
+            );
         } else {
             return null;
         }
