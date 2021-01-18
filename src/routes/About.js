07724503c1a372b748855aa0aeabfae8c40e3a0f
dal <res1206@naver.com>
2021-01-18 19:29:39 +0900

    리액트 끝 !

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css"
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>리액트 공부를 위한 페이지 !</span>
+            <span>2021-01-18</span>
+        </div>
+    );
+}
+
+export default About;
